7eac8e11ca8379fa68020fffbfc784a3b7d9228f
Chief Master <421401388@qq.com>
2018-10-16 22:49:32 +0800

    M 运算符词法分析器显示格式调整

--- mulanInterpreter/src/mini/Mini.java
+++ mulanInterpreter/src/mini/Mini.java
@@ -1,19 +1,21 @@
 package mini;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mini {
 
     public static void main(String[] args) {
-        //while(true) {
+        while(true) {
             LexicalAnalysis lexicalAnalysis = new LexicalAnalysis();
-            //Scanner in = new Scanner(System.in);
-            String input = "13#22";
+            Scanner in = new Scanner(System.in);
+//            String input = "13#22";
+            String input=in.next();
             ArrayList<Token> tokens=lexicalAnalysis.Lex(input);
             for (Token token:tokens) {
                 System.out.println(token.toString());
             }
-       // }
+        }
 
     }
 }

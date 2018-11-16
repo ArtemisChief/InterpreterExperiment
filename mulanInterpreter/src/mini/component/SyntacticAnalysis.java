c4dac24e785d6ab07086e9fe9888dabf42c4d620
AsrielMao <maomichael37@gmail.com>
2018-11-16 16:21:50 +0800

    语法分析还差连音、休止符和节奏

--- mulanInterpreter/src/mini/component/SyntacticAnalysis.java
+++ mulanInterpreter/src/mini/component/SyntacticAnalysis.java
@@ -19,13 +19,256 @@ public class SyntacticAnalysis {
         AbstractSyntaxTree = new Node("root");
 
         //Node score = parseScore();
+        while(tokens.get(index).getSyn()==2){
+            Node paragraph = parseParagraph();
+            AbstractSyntaxTree.addChild(paragraph);
+        }
+
+
+
         Node execution = parseExecution();
+        AbstractSyntaxTree.addChild(execution);
 
         //...
 
 
         return AbstractSyntaxTree;
     }
+
+    //paragraph -> 'paragraph' identifier speed tone { sentence } 'end'
+    public Node parseParagraph(){
+        Node paragraph = new Node("paragraph");
+        Node terminalNode;
+
+        //'paragraph',因为遇到'paragraph'才进入此函数，所以第一个不需要判断
+        terminalNode = new Node("paragraph","paragraph");
+        paragraph.addChild(terminalNode);
+        index++;
+
+        //identifier(段落名)
+        if(tokens.get(index).getSyn()!=100){
+            nextLine();
+            return new Node("Error", "缺少标识符");
+        }
+        terminalNode = new Node("identifier",tokens.get(index).getContent());
+        paragraph.addChild(terminalNode);
+        index++;
+
+        //speed & tone
+        Node speed,tone;
+        switch(tokens.get(index).getSyn()){
+            case 3:
+                speed=parseSpeed();
+                if(tokens.get(index).getSyn()!=4)
+                    tone=getTone();
+                else
+                    tone=parseTone();
+                break;
+            case 4:
+                tone=parseTone();
+                if(tokens.get(index).getSyn()!=3)
+                    speed=getSpeed();
+                else
+                    speed=parseSpeed();
+                break;
+            default:
+                speed=getSpeed();
+                tone=getTone();
+        }
+
+        paragraph.addChild(speed);
+        paragraph.addChild(tone);
+
+        //{ sentence }
+        while(tokens.get(index).getSyn()!=5){
+            Node sentence = parseSentence();
+            paragraph.addChild(sentence);
+        }
+
+        //'end',因为上一步sentence判断遇到end才会跳出，所以这一步肯定是end
+        terminalNode = new Node("end paragraph","end");
+        paragraph.addChild(terminalNode);
+        index++;
+
+
+        return paragraph;
+    }
+
+    //speed -> 'speed=' speedNum
+    public Node parseSpeed(){
+        Node speed = new Node("speed");
+        Node terminalNode;
+
+        //'speed='
+        terminalNode = new Node("speed mark","speed=");
+        speed.addChild(terminalNode);
+        index++;
+
+        //speed value
+        if(tokens.get(index).getSyn()!=96){
+            nextLine();
+            return new Node("Error", "速度常数不正确");
+        }
+        terminalNode = new Node("speed value",tokens.get(index).getContent());
+        speed.addChild(terminalNode);
+        index++;
+
+
+        return speed;
+    }
+
+    //tone -> ([#|b] toneValue)|toneValue
+    public Node parseTone(){
+        Node tone = new Node("tone");
+        Node terminalNode;
+
+        //'1='
+        terminalNode = new Node("tone mark","1=");
+        tone.addChild(terminalNode);
+        index++;
+
+        //#|b
+        if(tokens.get(index).getSyn()==18 | tokens.get(index).getSyn()==19){
+            terminalNode = new Node("lift mark",tokens.get(index).getContent());
+            tone.addChild(terminalNode);
+            index++;
+        }
+
+        //tone value
+        if(tokens.get(index).getSyn()!=95){
+            nextLine();
+            return new Node("Error", "调号不正确");
+        }
+        terminalNode = new Node("tone value",tokens.get(index).getContent());
+        tone.addChild(terminalNode);
+        index++;
+
+        return tone;
+    }
+
+    //sentence -> melody rhythm
+    public Node parseSentence(){
+        Node sentence = new Node("sentence");
+
+        //melody
+        Node melody = parseMelody();
+
+        //rhythm
+        Node rhythm = parseRhythm();
+
+        sentence.addChild(melody);
+        sentence.addChild(rhythm);
+
+        return sentence;
+    }
+
+    //melody -> { NotesInEight }
+    public Node parseMelody(){
+        Node melody = new Node("melody");
+
+        while(tokens.get(index).getSyn()!=13){
+            Node notesInEight = parseNotesInEight();
+            melody.addChild(notesInEight);
+        }
+
+        return melody;
+    }
+
+    //NotesInEight -> '(' Notes ')' | '[' Notes ']' | Notes
+    public Node parseNotesInEight(){
+        Node notesInEight = new Node("NotesInEight");
+
+        switch (tokens.get(index).getSyn()){
+            case 7:
+                notesInEight.addChild(new Node("lower left parentheses","("));
+                index++;
+
+                notesInEight.addChild(parseNotes());
+
+                if(tokens.get(index).getSyn()!=8){
+                    nextLine();
+                    return new Node("Error","缺少右括号");
+                }
+                notesInEight.addChild(new Node("lower right parentheses",")"));
+                index++;
+            case 9:
+                notesInEight.addChild(new Node("lower left parentheses","["));
+                index++;
+
+                notesInEight.addChild(parseNotes());
+
+                if(tokens.get(index).getSyn()!=10){
+                    nextLine();
+                    return new Node("Error","缺少右括号");
+                }
+                notesInEight.addChild(new Node("lower right parentheses","]"));
+                index++;
+            default:
+                notesInEight.addChild(parseNotes());
+        }
+
+        return notesInEight;
+    }
+
+    //Notes -> ([#|b] notesValue) | notesValue
+    public Node parseNotes(){
+        Node notes = new Node("Notes");
+        Node terminalNode;
+
+        //#|b
+        if(tokens.get(index).getSyn()==18 | tokens.get(index).getSyn()==19){
+            terminalNode = new Node("lift mark",tokens.get(index).getContent());
+            notes.addChild(terminalNode);
+            index++;
+        }
+
+        //notesValue
+        if(tokens.get(index).getSyn()!=98){
+            nextLine();
+            return new Node("Error", "音符不正确");
+        }
+        terminalNode = new Node("notes value",tokens.get(index).getContent());
+        notes.addChild(terminalNode);
+        index++;
+
+        return notes;
+    }
+
+    //rhythm ->
+    public Node parseRhythm(){
+        Node rhythm = new Node("Rhythm");
+
+        //...
+
+        return rhythm;
+    }
+
+    //get default speed if never set
+    public Node getSpeed(){
+        Node speed = new Node("speed");
+        Node terminalNode;
+
+        terminalNode = new Node("speed mark","speed=");
+        speed.addChild(terminalNode);
+        terminalNode = new Node("speed value","90");
+        speed.addChild(terminalNode);
+
+        return speed;
+    }
+
+    //get default tone if never set
+    public Node getTone(){
+        Node tone = new Node("tone");
+        Node terminalNode;
+
+        terminalNode = new Node("tone mark","1=");
+        tone.addChild(terminalNode);
+        terminalNode = new Node("tone value","C");
+        tone.addChild(terminalNode);
+
+        return tone;
+    }
+
 
     //execution -> play ( playlist )
     public Node parseExecution(){

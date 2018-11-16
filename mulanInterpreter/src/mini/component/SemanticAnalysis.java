5dcfb7f847d19dfde35d273a56e6cf40ccbf75b0
Chief Master <421401388@qq.com>
2018-11-16 22:13:31 +0800

    A 一点语义分析的说明，这部分就全都交给睿哥了

--- mulanInterpreter/src/mini/component/SemanticAnalysis.java
+++ mulanInterpreter/src/mini/component/SemanticAnalysis.java
@@ -17,9 +17,20 @@ public class SemanticAnalysis {
 
         //TODO 分析语法树转换成Arduino代码
 
-        //查表得到频率与时值，如
-        // String frequency = Note.Pitch.C;
-        // String duration = Note.Duration._g;
+        /**
+         * 关于速度
+         * 如果 speed=120，即每分钟120拍，即每拍持续0.5秒
+         * 默认四分音符为一拍，而四分音符时值为4，
+         * 则可以计算出Arduino中的时值因子为
+         * speedFactor=60/120/4*1000=125
+         * 其中只有120是变量，60为每分钟60秒，4为四分音符的时值，1000为秒转换成毫秒
+         */
+
+        /**
+         * 查表得到频率与时值，如
+         * String frequency = Note.Pitch.C;     (值为523)
+         * String duration = Note.Duration._g;  (值为1)
+         */
 
         return code;
     }

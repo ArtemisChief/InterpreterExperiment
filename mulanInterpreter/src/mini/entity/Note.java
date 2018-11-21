f8e74ace8e391c76679bd586459e0b956df9d3b2
Chief Master <421401388@qq.com>
2018-11-21 22:33:31 +0800

    M 语义分析音高频率已经完成，还剩下节奏和细节错误处理；采取用时间换空间的方法，字典类只留下基本音高频率和基本调性倍率，其他的通过计算得到

--- mulanInterpreter/src/mini/entity/Note.java
+++ mulanInterpreter/src/mini/entity/Note.java
@@ -3,86 +3,27 @@ package mini.entity;
 /**
  * 音符类
  * 相当于一个字典
- * 用查表的方法得到各个音的音高和时值持续的时间
+ * 用查表的方法得到基础音的频率与音调的倍率关系
  */
 
 public class Note {
     public class Pitch {
-        public static final String LC = "262";
-        public static final String LCS = "277";
-        public static final String LD = "294";
-        public static final String LDS = "311";
-        public static final String LE = "330";
-        public static final String LES = "349";
-        public static final String LF = "349";
-        public static final String LFS = "370";
-        public static final String LG = "392";
-        public static final String LGS = "415";
-        public static final String LA = "440";
-        public static final String LAS = "466";
-        public static final String LB = "494";
-        public static final String LBS = "523";
-
         public static final String C = "523";
-        public static final String CS = "554";
         public static final String D = "587";
-        public static final String DS = "622";
         public static final String E = "659";
-        public static final String ES = "698";
         public static final String F = "698";
-        public static final String FS = "740";
         public static final String G = "784";
-        public static final String GS = "831";
         public static final String A = "880";
-        public static final String AS = "932";
         public static final String B = "988";
-        public static final String BS = "1047";
-
-        public static final String HC = "1047";
-        public static final String HCS = "1109";
-        public static final String HD = "1175";
-        public static final String HDS = "1245";
-        public static final String HE = "1319";
-        public static final String HES = "1397";
-        public static final String HF = "1397";
-        public static final String HFS = "1480";
-        public static final String HG = "1568";
-        public static final String HGS = "1661";
-        public static final String HA = "1760";
-        public static final String HAS = "1865";
-        public static final String HB = "1976";
-        public static final String HBS = "2093";
-    }
-
-    public class Duration{
-        public static final String _1 = "16";
-        public static final String _2 = "8";
-        public static final String _4 = "4";
-        public static final String _8 = "2";
-        public static final String _g = "1";
     }
 
     public class Tonality{
-        public static final String CF="0.9439";
         public static final String C="1";
-        public static final String CS="1.0595";
-        public static final String DF="1.0595";
         public static final String D="1.1225";
-        public static final String DS="1.1892";
-        public static final String EF="1.1892";
         public static final String E="1.2599";
-        public static final String ES="1.3348";
-        public static final String FF="1.2599";
         public static final String F="1.3348";
-        public static final String FS="1.4142";
-        public static final String GF="1.4142";
         public static final String G="1.4983";
-        public static final String GS="1.5874";
-        public static final String AF="1.5874";
         public static final String A="1.6818";
-        public static final String AS="1.7818";
-        public static final String BF="1.7818";
         public static final String B="1.8877";
-        public static final String BS="2";
     }
 }

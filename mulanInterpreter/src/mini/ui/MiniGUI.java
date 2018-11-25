5fe3e2ca92d39ebba5086667365e005389081d81
Chief Master <421401388@qq.com>
2018-11-25 17:42:17 +0800

    A 增加使用提示、乐器表; M 修复play颜色，保持与paragraph和end一致，版本号升至1.1.7;

--- mulanInterpreter/src/mini/ui/MiniGUI.java
+++ mulanInterpreter/src/mini/ui/MiniGUI.java
@@ -4,6 +4,7 @@
 
 package mini.ui;
 
+import javafx.geometry.VerticalDirection;
 import mini.component.*;
 import mini.entity.Node;
 import mini.entity.Token;
@@ -75,8 +76,8 @@ public class MiniGUI extends JFrame {
         StyleConstants.setForeground(commentAttributeSet, new Color(128, 128, 128));
         StyleConstants.setForeground(errorAttributeSet, new Color(238, 0, 1));
         inputStyledDocument = inputTextPane.getStyledDocument();
-        statementPattern = Pattern.compile("\\bparagraph\\b|\\bend\\b");
-        keywordPattern = Pattern.compile("\\bspeed=|\\binstrument=|\\bvolume=|\\b1=|\\bplay");
+        statementPattern = Pattern.compile("\\bparagraph\\b|\\bend\\b|\\bplay");
+        keywordPattern = Pattern.compile("\\bspeed=|\\binstrument=|\\bvolume=|\\b1=");
         parenPattern = Pattern.compile("<(\\s*\\{?\\s*(1|2|4|8|g|\\*)+\\s*\\}?\\s*)+>");
 
         //关闭窗口提示
@@ -382,6 +383,7 @@ public class MiniGUI extends JFrame {
                     "//多声部同时播放\n" +
                     "play(Name1&Name2)";
             inputTextPane.setText(str);
+            inputTextPane.setCaretPosition(0);
             refreshColor();
 
             outputTextPane.setText("");
@@ -413,6 +415,7 @@ public class MiniGUI extends JFrame {
             }
             bufferedReader.close();
             inputTextPane.setText(stringBuilder.toString());
+            inputTextPane.setCaretPosition(0);
             outputTextPane.setText("");
             refreshColor();
             hasSaved = true;
@@ -686,6 +689,7 @@ public class MiniGUI extends JFrame {
             return;
 
         outputTextPane.setText(code);
+        outputTextPane.setCaretPosition(0);
 
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -731,6 +735,7 @@ public class MiniGUI extends JFrame {
             return;
 
         outputTextPane.setText(code);
+        outputTextPane.setCaretPosition(0);
 
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -927,6 +932,7 @@ public class MiniGUI extends JFrame {
             return;
 
         outputTextPane.setText(code);
+        outputTextPane.setCaretPosition(0);
 
         Random random = new Random(System.currentTimeMillis());
 
@@ -948,7 +954,7 @@ public class MiniGUI extends JFrame {
     //关于
     private void aboutMenuItemActionPerformed(ActionEvent e) {
         String str = "-----------------------------------------------------------\n" +
-                "Music Language Interpreter\nMade By Chief, yzdxm and AsrielMao\nVersion: 0.1.0\n\n" +
+                "Music Language Interpreter\nMade By Chief, yzdxm and AsrielMao\nVersion: 1.1.7\n\n" +
                 "A light weight interpreter for converting digit score       \n" +
                 "to Arduino code\n" +
                 "-----------------------------------------------------------";
@@ -1001,10 +1007,138 @@ public class MiniGUI extends JFrame {
                 "//双声部同时播放\n" +
                 "play(soprano&alto)";
         inputTextPane.setText(str);
+        inputTextPane.setCaretPosition(0);
         outputTextPane.setText("");
         refreshColor();
         hasChanged = false;
         this.setTitle("Music Interpreter - Demo");
+    }
+
+    //显示提示
+    private void TipsMenuItemActionPerformed(ActionEvent e) {
+        String str = "============================================\n" +
+                "                                                  Tips\n" +
+                "-------------------------------------------------------------------------\n" +
+                "\n" +
+                "1. 构成乐谱的成分：\n" +
+                "\t1）paragraph Name  声部声明\n" +
+                "\t2）instrument= 0      \t演奏的乐器（非必要 默认钢琴）\n" +
+                "\t3）volume= 127        该声部的音量（非必要 默认127）\n" +
+                "\t4）speed= 90\t该声部演奏速度（非必要 默认90）\n" +
+                "\t5）1= C\t\t该声部调性（非必要 默认C调）\n" +
+                "\t6）((1))(2)3[4][[5]]\t音符的音名，即音高\n" +
+                "\t7）<1248g*>\t音符的时值，即持续时间\n" +
+                "\t8）end\t\t声部声明结束\n" +
+                "\t（其中2~3，即乐器与音量只对Midi有效）\n" +
+                "\n" +
+                "2. 乐谱成分的解释：\n" +
+                "\t1）声部声明：标识符须以字母开头，后跟字母或数字\n" +
+                "\t2）乐器音色：见 帮助-乐器 中具体说明\n" +
+                "\t3）声部音量：最小值0（禁音）最大值127（最大音量）\n" +
+                "\t4）声部速度：每分钟四分音符个数，即BPM\n" +
+                "\t5）声部调性：CDEFGAB加上b（降号）与#（升号）\n" +
+                "\t6）“( )”内为低八度，可叠加“[ ]”内为高八度，同上\n" +
+                "\t7）“< >”内为全、二、四、八、十六分音符与附点\n" +
+                "\t8）声明结束：须用end结束声明，对应paragraph\n" +
+                "\n" +
+                "3. 播放乐谱的方法：\n" +
+                "\t1）通过“play( )”进行播放\n" +
+                "\t2）“( )”内为声部的标识符\n" +
+                "\t3）“&”左右的声部将同时播放，\n" +
+                "\t4）“ , ”左右的声部将先后播放";
+        outputTextPane.setText(str);
+        outputTextPane.setCaretPosition(0);
+    }
+
+    //显示乐器列表
+    private void InstruMenuItemActionPerformed(ActionEvent e) {
+        String str = "===========================================\n" +
+                "                                            Instrument\n" +
+                "-----------------------------------------------------------------------\n" +
+                "音色号\t乐器名\t                |\t音色号\t乐器名\n" +
+                "-----------------------------------------------------------------------\n" +
+                "钢琴类\t\t                |\t簧乐器\n" +
+                "0\t大钢琴\t                |\t64\t高音萨克斯\n" +
+                "1\t亮音钢琴\t                |\t65\t中音萨克斯\n" +
+                "2\t电子大钢琴\t                |\t66\t次中音萨克斯\n" +
+                "3\t酒吧钢琴\t                |\t67\t上低音萨克斯\n" +
+                "4\t电钢琴1\t                |\t68\t双簧管\n" +
+                "5\t电钢琴2\t                |\t69\t英国管\n" +
+                "6\t大键琴\t                |\t70\t巴颂管\n" +
+                "7\t电翼琴\t                |\t71\t单簧管\n" +
+                "-----------------------------------------------------------------------\n" +
+                "固定音高敲击乐器\t                |\t吹管乐器\n" +
+                "8\t钢片琴\t                |\t72\t短笛\n" +
+                "9\t钟琴\t                |\t73\t长笛\n" +
+                "10\t音乐盒\t                |\t74\t竖笛\n" +
+                "11\t颤音琴\t                |\t75\t牧笛\n" +
+                "12\t马林巴琴\t                |\t76\t瓶笛\n" +
+                "13\t木琴\t                |\t77\t尺八\n" +
+                "14\t管钟\t                |\t78\t哨子\n" +
+                "15\t洋琴\t                |\t79\t陶笛\n" +
+                "-----------------------------------------------------------------------\n" +
+                "风琴\t\t                |\t合成音主旋律\n" +
+                "16\t音栓风琴\t                |\t80\t方波\n" +
+                "17\t敲击风琴\t                |\t81\t锯齿波\n" +
+                "18\t摇滚风琴\t                |\t82\t汽笛风琴\n" +
+                "19\t教堂管风琴\t                |\t83\t合成吹管\n" +
+                "20\t簧风琴\t                |\t84\t合成电吉他\n" +
+                "21\t手风琴\t                |\t85\t人声键\n" +
+                "22\t口琴\t                |\t86\t五度音\n" +
+                "23\t探戈手风琴\t\t87\t贝斯吉他合奏\n" +
+                "-----------------------------------------------------------------------\n" +
+                "吉他\t\t                |\t合成音和弦衬底\n" +
+                "24\t木吉他（尼龙弦）      |\t88\t新时代\n" +
+                "25\t木吉他（钢弦）          |\t89\t温暖的\n" +
+                "26\t电吉他（爵士）          |\t90\t多重和音\n" +
+                "27\t电吉他（清音）          |\t91\t唱诗班\n" +
+                "28\t电吉他（闷音）          |\t92\t弓弦音色\n" +
+                "29\t电吉他（驱动音效）   |\t93\t金属的\n" +
+                "30\t电吉他（失真音效）   |\t94\t光华\n" +
+                "31\t吉他泛音\t                |\t95\t宽阔的\n" +
+                "-----------------------------------------------------------------------\n" +
+                "贝斯\t\t                |\t合成音效果\n" +
+                "32\t贝斯\t                |\t96\t雨声\n" +
+                "33\t电贝斯（指弹）          |\t97\t电影音效\n" +
+                "34\t电贝斯（拨片）          |\t98\t水晶\n" +
+                "35\t无品贝斯\t                |\t99\t气氛\n" +
+                "36\t打弦贝斯1\t                |\t100\t明亮\n" +
+                "37\t打弦贝斯2\t                |\t101\t魅影\n" +
+                "38\t合成贝斯1\t                |\t102\t回音\n" +
+                "39\t合成贝斯2\t                |\t103\t科幻\n" +
+                "-----------------------------------------------------------------------\n" +
+                "弦乐器\t\t                |\t民族乐器\n" +
+                "40\t小提琴\t                |\t104\t西塔琴\n" +
+                "41\t中提琴\t                |\t105\t斑鸠琴\n" +
+                "42\t大提琴\t                |\t106\t三味线\n" +
+                "43\t低音提琴\t                |\t107\t古筝\n" +
+                "44\t颤弓弦乐\t                |\t108\t卡林巴铁片琴\n" +
+                "45\t弹拨弦乐\t                |\t109\t苏格兰风琴\n" +
+                "46\t竖琴\t                |\t110\t古提亲\n" +
+                "47\t定音鼓\t                |\t111\t兽笛\n" +
+                "-----------------------------------------------------------------------\n" +
+                "合奏\t\t                |\t打击乐器\n" +
+                "48\t弦乐合奏1\t                |\t112\t叮当铃\n" +
+                "49\t弦乐合奏2\t                |\t113\t阿果果鼓\n" +
+                "50\t合成弦乐1\t                |\t114\t钢鼓\n" +
+                "51\t合成弦乐2\t                |\t115\t木鱼\n" +
+                "52\t唱诗班“啊”             |\t116\t太鼓\n" +
+                "53\t合唱“喔”\t                |\t117\t定音筒鼓\n" +
+                "54\t合成人声\t                |\t118\t合成鼓\n" +
+                "55\t交响打击乐\t                |\t119\t反钹\n" +
+                "-----------------------------------------------------------------------\n" +
+                "铜管乐器\t\t                |\t特殊音效\n" +
+                "56\t小号\t                |\t120\t吉他滑弦杂音\n" +
+                "57\t长号\t                |\t121\t呼吸杂音\n" +
+                "58\t大号\t                |\t122\t海浪\n" +
+                "59\t闷音小号\t                |\t123\t鸟鸣\n" +
+                "60\t法国圆号\t                |\t124\t电话铃声\n" +
+                "61\t铜管乐\t                |\t125\t直升机\n" +
+                "62\t合成铜管1\t                |\t126\t鼓掌\n" +
+                "63\t合成铜管2\t                |\t127\t枪声\n" +
+                "===========================================";
+        outputTextPane.setText(str);
+        outputTextPane.setCaretPosition(0);
     }
 
     private void initComponents() {
@@ -1028,6 +1162,8 @@ public class MiniGUI extends JFrame {
         generateMidiMenuItem = new JMenuItem();
         playMenuItem = new JMenuItem();
         helpMenu = new JMenu();
+        InstruMenuItem = new JMenuItem();
+        TipsMenuItem = new JMenuItem();
         demoMenuItem = new JMenuItem();
         aboutMenuItem = new JMenuItem();
         hSpacer1 = new JPanel(null);
@@ -1143,6 +1279,16 @@ public class MiniGUI extends JFrame {
             {
                 helpMenu.setText("Help");
 
+                //---- InstruMenuItem ----
+                InstruMenuItem.setText("Instrument");
+                InstruMenuItem.addActionListener(e -> InstruMenuItemActionPerformed(e));
+                helpMenu.add(InstruMenuItem);
+
+                //---- TipsMenuItem ----
+                TipsMenuItem.setText("Tips");
+                TipsMenuItem.addActionListener(e -> TipsMenuItemActionPerformed(e));
+                helpMenu.add(TipsMenuItem);
+
                 //---- demoMenuItem ----
                 demoMenuItem.setText("Demo");
                 demoMenuItem.addActionListener(e -> demoMenuItemActionPerformed(e));
@@ -1242,6 +1388,8 @@ public class MiniGUI extends JFrame {
     private JMenuItem generateMidiMenuItem;
     private JMenuItem playMenuItem;
     private JMenu helpMenu;
+    private JMenuItem InstruMenuItem;
+    private JMenuItem TipsMenuItem;
     private JMenuItem demoMenuItem;
     private JMenuItem aboutMenuItem;
     private JPanel hSpacer1;
